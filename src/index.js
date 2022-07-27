6e039c3bab6bcb632769f2c8dba1f1b1c754fa10
David Nelson <larva.code15@gmail.com>
2022-07-27 04:37:58 +0100

    Added PWA Feature

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { render } from 'react-snapshot';
 import { BrowserRouter as Router } from "react-router-dom";
-import 'https://res.cloudinary.com/ruthless-labs/raw/upload/v1658892190/bootstrap_bl7lda.css';
+import './assets/bootstrap.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'aos/dist/aos.css';
 import './index.css';
